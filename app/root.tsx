f06aa21157aac0725df6bd84e564ceeafc35f670
Taz <travontazlowry@gmail.com>
2024-06-24 10:51:54 -0400

    we broke it

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,19 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from "./styles.css";
 import "./tailwind.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://unpkg.com/modern-normalize@1.1.0/modern-normalize.css",
+    },
+    { rel: "stylesheet", href: styles },
+  ];
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,76 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Remix Newsletter" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
-export default function Index() {
+import { ActionFunction } from "@remix-run/node";
+import { Form, Link, useActionData, useNavigation } from "@remix-run/react";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+
+  const API_KEY = process.env.CONVERTKIT_KEY;
+  const FORM_ID = "6746889";
+  const API = "https://api.convertkit.com/v3/forms/";
+
+  const res = await fetch(`${API}/forms/${FORM_ID}/subscribe`, {
+    method: "post",
+    body: JSON.stringify({ email, api_key: API_KEY }),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+
+  return res.json();
+};
+
+export default function Newsletter() {
+  const actionData = useActionData();
+  const transition = useNavigation();
+  const state: "idle" | "success" | "error" | "submitting" =
+    transition.submission
+      ? "submitting"
+      : actionData?.subscription
+      ? "success"
+      : actionData?.error
+      ? "error"
+      : "idle";
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <Form method="post" aria-hidden={state === "success"}>
+        <div>
+          <h2>Subscribe!</h2>
+          <p>Dont miss any of the action</p>
+          <fieldset>
+            <input
+              type="email"
+              name="email"
+              placeholder="you@example.com"
+              className="rounded-lg p-2 m-2"
+            />
+            <button type="submit">Subscribe</button>
+          </fieldset>
+
+          <p>
+            {(actionData as { error?: string })?.error ? (
+              (actionData as { message?: string })?.message
+            ) : (
+              <>&nbsp;</>
+            )}
+          </p>
+        </div>
+      </Form>
+
+      <div aria-hidden={state !== "success"}>
+        <h2>You are now subscribed to the newsletter!</h2>
+        <p>Please check your email to confirm your subscribtion</p>
+        <Link to=".">Start over</Link>
+      </div>
+    </main>
   );
 }
